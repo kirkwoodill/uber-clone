82e683ac2b6ba666417cdd7e324703be5dc7f35b
Kirk Woodill <zkcaptkirk@gmail.com>
2021-10-26 16:40:24 +1300

    Add GooglePlacesAutocomplete & react-native-dotenv

--- App.js
+++ App.js
@@ -4,14 +4,32 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screen/HomeScreen";
+import MapScreen from "./screen/MapScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
@@ -19,6 +20,8 @@ const options =[
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
     return (
         <FlatList
             data = {options}
@@ -26,6 +29,7 @@ const NavOptions = () => {
             keyExtractor = {(item) => item.id}
             renderItem = {({ item }) => (
                 <TouchableOpacity
+                    onPress = {() => navigation.navigate(item.screen)}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
                 >
                     <View>
