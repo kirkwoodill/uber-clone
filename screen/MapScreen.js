e0c02d66d8c2466cfd025c143acb3f2ac8fa2a90
Kirk Woodill <zkcaptkirk@gmail.com>
2021-11-01 21:52:35 +1300

    Implement map screen

--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Map from '../components/Map';
 
 const MapScreen = () => {
     return (
-        <View>
-            <Text>This is ze map screen</Text>
+      <View>
+        <Text>This is ze map screen</Text>
+        <View style={tw`h-1/2`}>
+            <Map />
         </View>
-    )
+        <View style={tw`h-1/2`}></View>
+      </View>
+    );
 }
 
 export default MapScreen
